c4510f05a5a5d073707b73757aca10dfd1e71e01
Mike Kipp <168943773+shadowlord1012@users.noreply.github.com>
2024-08-05 12:37:20 -0400

    Add files via upload

--- Final Project/js/JSforpages.js
+++ Final Project/js/JSforpages.js
@@ -2,5 +2,18 @@ $(document).ready(function () {
     $(function (){
         $("#head").load("metadata.html");
         $("#header").load("header.html");
+        $("#footer").load("footer.html");
     });
+
+    $(".button_Base_Buy").click(function()
+    {
+        alert("Discontinued! Try ebay.");
+    });
+    $(".Button_Mobile_Buy").click(function(){
+        alert("Discontinued! Try ebay.");
+    });
+    $(".Button_Shuttle_Buy").click(function(){
+        alert("Discontinued! Try ebay.");
+    })
+
 });
